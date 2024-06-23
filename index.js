8abad6fad508df93f6040ca854615562940b66cc
Claudiocci3 <quevedoclaudio99@gmail.com>
2024-06-22 23:29:33 -0300

    js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const project = document.getElementById('project');
 const contact = document.getElementById('contact');
 
 
-  function scrollSkill(e) {
-    e.preventDefault()
+  function scrollSkill(event) {
+    event.preventDefault()
     window.scroll(0, 590);
   }
 
